60fcc0fb849dff12941aaa2b06ce2b32d75bc704
Alexander <alex2.aslund@gmail.com>
2021-04-19 18:28:21 +0200

    runtime tests

--- dslab1part2/dslab1part2.cpp
+++ dslab1part2/dslab1part2.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <queue>
 #include <stack>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 
 
@@ -13,9 +15,9 @@ bool testPalStack(string s) {
         t.push(s[i]);
     }
     isPalindrome = true;
-    for (int i = 1; i <= s.size() && isPalindrome; i++)
+    for (int i = 1; i <= s.size() && isPalindrome; i++) //breaks if isPalindrome is false
     {
-        isPalindrome = t.top() == s[s.size() - i];
+        isPalindrome = t.top() == s[s.size() - i]; //isPalindrome is set through comparing strings
         t.pop();
     }
     return isPalindrome;
@@ -23,14 +25,13 @@ bool testPalStack(string s) {
 
 bool testPalQueue(string s) {
     queue <char> q;
-    bool isPalindrome;
+    bool isPalindrome = true;
     for (int i = 0; i < s.size(); i++) {
         q.push(s[i]);
     }  
-    isPalindrome = true;
-    for (int i = 1; i <= s.size() && isPalindrome; i++)
+    for (int i = 1; i <= s.size() && isPalindrome; i++) //breaks if isPalindrome is false
     {
-        isPalindrome = q.front() == s[s.size() - i];
+        isPalindrome = q.front() == s[s.size() - i]; //isPalindrome is set through comparing strings
         q.pop();
     }
     return isPalindrome;
@@ -54,10 +55,23 @@ string fixText(string s) {
 }
 int main()
 {
-    string str;
-    cout << "Enter test: ";
-    getline(cin, str);
-    str = fixText(str);
-    //cout << "it is: " << boolalpha << testPalQueue(str);
-    cout << "it is: " << boolalpha << testPalStack(str);
+    while (true) {
+        string str;
+        cout << "Enter test: ";
+        getline(cin, str);
+        str = fixText(str);
+        //cout << "it is: " << boolalpha << testPalQueue(str); //Queue
+        //cout << "it is: " << boolalpha << testPalStack(str); //Stack
+        high_resolution_clock::duration totalTime(0);
+        for (int i = 0; i < 1000; i++) {
+            auto start = high_resolution_clock::now();
+            //testPalStack(str); //21000 ~ 25000 at 1000 iterations
+            testPalQueue(str); //19000 ~ 23000 at 1000 iterations
+            auto end = high_resolution_clock::now();
+            totalTime = totalTime + (end - start);
+        }
+        auto average = totalTime.count() / 1000;
+        cout << "the average time is: " << average << " nanoseconds\n";
+    }
+   
 }
